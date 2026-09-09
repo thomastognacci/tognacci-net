d7c0fde2b99fdb2ee3700127dfc57161fd843eda
Thomas Tognacci <thomas.tognacci@flamefy.com>
2026-09-10 00:17:04 +0200

    update github page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://thomas.tognacci.net',
+  site: 'https://tognacci.net',
   output: 'static',
   devToolbar: { enabled: false },
 });
